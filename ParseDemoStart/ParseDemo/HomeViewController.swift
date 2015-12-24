e85a526035847e04157164e31936eda88f4611c1
Brandon Aubrey <aubrey-brandon@aramark.com>
2015-12-24 10:00:24 -0500

    first commit

---
+++ ParseDemoStart/ParseDemo/HomeViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+import Parse
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Show the current visitor's username
+        if let pUserName = PFUser.currentUser()?["username"] as? String {
+            self.userNameLabel.text = "@" + pUserName
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        if (PFUser.currentUser() == nil) {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
+                self.presentViewController(viewController, animated: true, completion: nil)
+            })
+        }
+    }
+    
+    @IBAction func logOutAction(sender: AnyObject){
+        
+        // Send a request to log out a user
+        PFUser.logOut()
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login") 
+            self.presentViewController(viewController, animated: true, completion: nil)
+        })
+        
+    }
+
+}
